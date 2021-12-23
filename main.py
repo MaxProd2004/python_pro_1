641bb8654a880966a82ba550fda29e535fe92c3a
MaxPrykhodko <87067987+MaxProd2004@users.noreply.github.com>
2021-12-23 22:33:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,55 @@
-# Задание 1
-print("Задание 1")
-class Product_description:
-    def __init__(self, price, description, dimensions):
+class Product:
+    """Class, which describe product"""
+
+    def __init__(self, title, price, description=''):
+        """Initialize attributes Product's instances"""
+        self.title = title
         self.price = price
         self.description = description
-        self.dimensions = dimensions
+
     def __str__(self):
-        return "Product [price = {}, description = {}, dimensions = {}]".format(self.price, self.description, self.dimensions)
+        return f'{self.title}: {self.price} грн'
 
-product_1 = Product_description(1000, "This is computer", 5)
-product_2 = Product_description(2500, "This is fridge", 100)
-print(product_1)
-print(product_2, '\n')
 
-# Задание 2
-print("Задание 2")
 class Customer:
-    def __init__(self, name, surname, patronymic, phone_number):
+    """Class, which describe customer"""
+
+    def __init__(self, name, surname, phone):
+        """Initialize attributes Customer's instances"""
         self.name = name
         self.surname = surname
-        self.patronymic = patronymic
-        self.phone_number = phone_number
-    def __str__(self):
-        return "Info about customer: [name = {}, surname = {}, patronymic = {}, phone_number = {}]".format(self.name, self.surname, self.patronymic, self.phone_number)
-customer_1 = Customer("Ivan", "Ivanov", "Vasilievich", "+380965761204")
-print(customer_1, '\n')
+        self.phone = phone
 
+    def __str__(self):
+        return f'{self.surname} {self.name[0]}., {self.phone}'
+
+class Order:
+    "Class, which describe order"
+
+    def __init__(self, customer: Customer, products = None):
+        self.customer = customer
+        self.products = products
+
+    def __str__(self):
+        res = '\n'.join(map(str, self.products))
+        return f'{self.customer}\n{res}'
+
+    def total_price(self):
+        s = 0
+        for item in self.products:
+            s += item.price
+        return s
+
+if __name__ == '__main__':
+    x = Product('apple', 23)
+    y = Product('banana', 25)
+    z = Product('orange', 22)
+
+    customer_1 = Customer('Ivan', 'Ivanov', '050689438')
+    customer_2 = Customer('Petr', 'Petrov', '067892410')
+
+    order_1 = Order(customer_1, [x, y, z])
+    order_2 = Order(customer_2, [x, z])
+
+    print(order_1)
+    print(order_2)
